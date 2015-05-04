1e742f1ce0100384f718f997a238602314b15522
alaasayed <alaa.sayed4@gmail.com>
2015-05-04 20:10:25 +0300

    translation

---
+++ Consolebingapp/Consolebingapp/main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolebingapp
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Run...");
+            //string result = Translator.translate("en", "كتاب", "ar");
+            string result = Translator.translate("en","الكتاب على الطاولة","ar");
+            Console.WriteLine(result);
+            result = Translator.translate("en", "الكلب يجري في الحديقة", "ar");
+            Console.WriteLine(result);
+
+            Console.ReadKey(true);
+        }
+    }
+}
